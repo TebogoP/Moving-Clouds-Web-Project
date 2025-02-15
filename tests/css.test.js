e594004a79acee0356c8e991eb550723508fbd1a
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-02-15 21:06:51 +0200

    Added more css test for container

--- tests/css.test.js
+++ tests/css.test.js
@@ -24,11 +24,43 @@ describe('CSS Tests', () => {
         dom.window.document.head.appendChild(styleElement);
     });
 
+//CSS Test for body
     test('body should have margin set to 0px', () => {
         const body = dom.window.document.body;
         const styles = dom.window.getComputedStyle(body);
+        expect(styles.margin).toBe('0px');// Check if the margin is set to 0px
+    });
 
-        // Check if the margin is set to 0px
-        expect(styles.margin).toBe('0px');
+//CSS Test for container
+    // CSS Test for container height
+    test('container div should have height set to 100vh', () => {
+        const container = dom.window.document.querySelector('#container')
+        const styles = dom.window.getComputedStyle(container);
+        expect(styles.height).not.toBeNull();
+        expect(styles.height).toBe('100vh');
+    });
+
+    // CSS Test for container position
+    test('container div should have position set to relative', () => {
+        const container = dom.window.document.querySelector('#container')
+        const styles = dom.window.getComputedStyle(container);
+        expect(styles.position).not.toBeNull();
+        expect(styles.position).toBe('relative');
+    });
+
+    // CSS Test for container background-color
+        test('container div should have background-color set to blue', () => {
+            const container = dom.window.document.querySelector('#container')
+            const styles = dom.window.getComputedStyle(container);
+            expect(styles.backgroundColor).not.toBeNull();
+            expect(styles.backgroundColor).toBe('rgb(0, 0, 255)');
+        });
+
+    // CSS Test for container overflow
+    test('container div should have overflow set to hidden', () => {
+        const container = dom.window.document.querySelector('#container')
+        const styles = dom.window.getComputedStyle(container);
+        expect(styles.overflow).not.toBeNull();
+        expect(styles.overflow).toBe('hidden');
     });
 });
