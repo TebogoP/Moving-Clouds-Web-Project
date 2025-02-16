fe2f2f1b6814b3eba3ff1315b6a122b25841a95e
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-02-16 09:49:16 +0200

    Added Test for container child -> with class cloud styling using JavaScript

---
+++ tests/script.test.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+const { JSDOM } = require('jsdom');
+
+// Load the HTML file
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+
+// Load the JS file
+const js = fs.readFileSync(path.resolve(__dirname, '../js/script.js'), 'utf8');
+
+describe('JS Tests', () => {
+    let dom;
+
+    beforeAll(() => {
+        // Create a DOM environment
+        dom = new JSDOM(html, {
+            runScripts: 'dangerously', // Allow scripts to run
+            resources: 'usable', // Allow loading external resources (e.g., CSS,JS ect.)
+        });
+
+        // Add JS to the DOM
+        const scriptElement = dom.window.document.createElement('script');
+        scriptElement.textContent = js;
+        dom.window.document.body.appendChild(scriptElement);
+    });
+
+//Test for setting .cloud element top and left properties using script.js
+    test('first .cloud element should have left and top styles set to 10px', () => {
+    dom.window.document.dispatchEvent(new dom.window.Event('DOMContentLoaded')); // Simulate the DOMContentLoaded event
+    const cloud = dom.window.document.querySelector('.cloud'); // Select the first .cloud element
+    expect(cloud).not.toBeNull();// Ensure the cloud element exists
+
+    const styles = dom.window.getComputedStyle(cloud); // Get the computed styles of the cloud
+    expect(styles.left).toBe('10px');// Check that the left  property is set to 10px
+    expect(styles.top).toBe('10px');// Check that the top property is set to 10px
+    });
+
+});
