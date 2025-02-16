bac009356070d1d2a98db5b3837d48f210b624fd
TebogoP <33540200+TebogoP@users.noreply.github.com>
2025-02-16 10:50:23 +0200

    Added container child -> with class cloud styling using JavaScript

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const elements = document.getElementsByClassName('cloud');
+    const element = elements[0];
+    element.style.left = '10px';  // or any value greater than 0px
+    element.style.top = '10px';
+});
